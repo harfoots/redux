9acd228ecfcfba18b45f5d42a06d15ceb01303e4
Thomas Uster <thomasuster@gmail.com>
2023-07-20 22:05:15 -0700

    Update examples/shopping-cart/src/index.js:

--- examples/shopping-cart/src/index.js
+++ examples/shopping-cart/src/index.js
@@ -1,5 +1,6 @@
+migrations/test/harfoots/redux/examples/shopping-cart/src/index.js
 import React from 'react'
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import { createLogger } from 'redux-logger'
@@ -20,9 +21,10 @@ const store = createStore(
 
 store.dispatch(getAllProducts())
 
-render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 )
