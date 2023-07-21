a8329cbb46a486c86b85849bf98315899db36092
Thomas Uster <thomasuster@gmail.com>
2023-07-21 04:27:30 -0700

    Update examples/counter-ts/src/App.test.tsx:

--- examples/counter-ts/src/App.test.tsx
+++ examples/counter-ts/src/App.test.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { createRoot } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(
+  const container = document.getElementById('app');
+  const root = createRoot(container);
+  root.render(
     <Provider store={store}>
       <App />
     </Provider>
